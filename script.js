cd697621f0867539d80654e9ca7dd6f66e26d883
Mirza Ahmed Zubayer <azubayer632@gamil.com>
2026-03-09 13:18:23 +0600

    added login functionality

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // login functionality
+const form = document.getElementById("login-form");
+
+form.addEventListener("submit", (event)=>{
+    event.preventDefault();
+
+    const userName = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    const demoUsername = "admin";
+    const demoPassword = "admin123"
+
+    if(userName === demoUsername && password === demoPassword){
+        window.location.href = "issues.html"
+    }else{
+        window.alert(`Username or Password is incorrect`)
+    }
+})
